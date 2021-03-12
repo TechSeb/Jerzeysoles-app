31c675b378c2769da6e525c4320399e39c887bd9
Seb <stapia1@njcu.edu>
2021-03-12 13:57:32 -0500

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,65 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'Nike Off White 1',
+            category: 'Shoes',
+            image: '/images/OW1Chi.jpg',
+            price: 190,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product',
+
+        },
+        {
+            _id:'2',
+            name: 'Yeezy V3',
+            category: 'Shoes',
+            image: '/images/OGYZYV3.jpg',
+            price: 200,
+            brand: 'Adidas',
+            rating: 4,
+            numReviews: 8,
+            description: 'high quality product',
+
+        },
+        {
+            _id:'3',
+            name: 'Jordan 11s',
+            category: 'Shoes',
+            image: '/images/AJ11Con.jpg',
+            price: 220,
+            brand: 'Jordan',
+            rating: 3,
+            numReviews: 13,
+            description: 'high quality product',
+
+        },
+        {
+            _id:'4',
+            name: 'Nike Off White 1',
+            category: 'Shoes',
+            image: '/images/OW1Chi.jpg',
+            price: 190,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product',
+
+        },
+        {
+            _id:'5',
+            name: 'Nike Off White 1',
+            category: 'Shoes',
+            image: '/images/OW1Chi.jpg',
+            price: 190,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product',
+
+        },
+    ],
+};
+export default data;
